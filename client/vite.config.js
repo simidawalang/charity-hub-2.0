c0675724e93785f3b6ea7ba4f841f2e561aa691c
L I L V C <simidawalang@gmail.com>
2023-10-30 18:01:19 +0100

    imported assets, tailwind setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react(), nodePolyfills(), tailwindcss()],
   define: {
     "process.env": {},
   },
